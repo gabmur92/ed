9b1effef0064942a68538007bcd93905d28fb09c
gabmur92 <gabmur@alu.iesserpis.org>
2019-10-21 13:49:53 +0200

    mejorado JAdivina

--- JAdivina/src/iesserpis/ed/Adivina.java
+++ JAdivina/src/iesserpis/ed/Adivina.java
@@ -6,47 +6,48 @@ public class Adivina {
 
 	public static void main(String[] args) {
 		Scanner tcl=new Scanner(System.in);
-		int x=(int)(Math.random()*1000+1);// un numero aleatorio entre el 1 y el 1000
+// un numero aleatorio entre el 1 y el 1000
+		int x=(int)(Math.random()*1000);
 		int intentos=1;
 		int num=0;
 		
-	
+		System.out.println(x);
 		System.out.println("Adivina un numero entre 1 y 1000 ");
-		//para entrar en el programa  tienes que introducir un numero diferente de la respuesta, si no saldra
-		while(num != x) {
+//para entrar en el programa  tienes que introducir un numero diferente de la respuesta, si no saldra
+		while(num != x&&intentos<25) {
 			num= tcl.nextInt();
-			// si aciertas entrara aqui si no saltara
+// si aciertas entrara aqui si no saltara
 			if(num==x) {
-				//dependiendo de los intentos  te dara una respuesta u otra
+//dependiendo de los intentos  te dara una respuesta u otra
 				if (intentos<=5) {
-					System.out.println("Felicidades eres un maquina, as acertado con solo "+intentos+" intentos");
-				}
+					System.out.println("Felicidades eres un maquina, as acertado con solo "+intentos+" intentos");	
+					}
 				else if(intentos>5 && intentos<=10) {
-					System.out.println("No esta mal con "+intentos+" intentos");
-				}
+					System.out.println("No esta mal con "+intentos+" intentos");	
+					}
 				else if(intentos>10 && intentos<=15) {
-					System.out.println("Bueno "+intentos+" intentos podria ser mejorable...");
-				}
+					System.out.println("Bueno "+intentos+" intentos podria ser mejorable...");	
+					}
 				else {
-					System.out.println(intentos+"..... Dedicate a otra cosa, te ira mejor");
-
-				}
-								
-		}
-			//si no has acertado comprobara si es mayor o menor
+					System.out.println(intentos+"..... Dedicate a otra cosa, te ira mejor");	
+					}							
+			}
+//si no has acertado comprobara si es mayor o menor
 			else if(num>x) {
-				System.out.println("el numero "+num+" es mayor");
-									
-		}
-			else{ 
-				System.out.println("el numero "+num+" es menor");
-								
-		}
-			intentos++;
+				System.out.println("El numero "+num+" es mayor");	
+				}
+			else { 
+				System.out.println("El numero "+num+" es menor");	
+				}		
+			intentos++;		
 			
 		}
-		System.out.println("fin de programa");
-
+		if(intentos<25) {
+		System.out.println("Has ganado, Fin de programa");
+		}
+		else
+			System.out.println("Demasiados intentos Has perdido, Fin de programa");
 	}
-
 }
+
+
--- JVector/src/iesserpis/ed/Vector.java
+++ JVector/src/iesserpis/ed/Vector.java
@@ -7,7 +7,7 @@ public class Vector {
 		
 		int[] v = {17, 12, 15, 9, 14};
 		for(int index=0; index<5;index++)
-		System.out.printf("v[%s] = %s %n",index,v[index]);
+		System.out.printf("elemento "+index+" valor = "+ v[index]);
 //		System.out.println("v[1] = "+v[1]);
 //		System.out.println("v[2] = "+v[2]);
 		
