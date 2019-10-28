aad7118248d63f6794395954be3ece4d07942e60
gabmur92 <gabmur@alu.iesserpis.org>
2019-10-28 13:45:48 +0100

    jvector explicado

--- JVector/src/iesserpis/ed/Vector.java
+++ JVector/src/iesserpis/ed/Vector.java
@@ -4,13 +4,31 @@ public class Vector {
 
 	public static void main(String[] args) {
 //		int index=0;
+//		
+//		int[] v = new int[]{17, 12, 15, 9, 14};
+//
+//	//	for(index=0; index<5;index++) {
+// 		//System.out.println("elemento "+index+" valor = "+ v[index]);
+		int[] v = new int[]{17, 12, 15, 9, 14};
+		int x=33;
+		int index= indexOf(v,x);
 		
-		int[] v = {17, 12, 15, 9, 14};
-		for(int index=0; index<5;index++)
-		System.out.printf("elemento "+index+" valor = "+ v[index]);
-//		System.out.println("v[1] = "+v[1]);
-//		System.out.println("v[2] = "+v[2]);
+		System.out.println("index "+index);
+		System.out.println("fin");
+	
+	}
+		
+	public static int indexOf(int[] v, int x) {
+		
+		int index=0;
+		while(index<v.length && v[index]!=x) 
+			index++;
+			if(index==v.length) { 
+				return -1;
+			}
+			return index;
+		
 		
 	}
-
+	
 }
